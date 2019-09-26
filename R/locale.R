2286a6ae72a564d139b9628138bfd3ac71791efe
Milos Basaraba <milos.basaraba@gmail.com>
2019-09-26 17:40:48 +0200

    Sys locale read change

--- R/locale.R
+++ R/locale.R
@@ -1,10 +1,9 @@
 myenv$def_loc <- Sys.getlocale()
-myenv$def_loc_arg <- stringr::str_extract(myenv$def_loc, "(?<=LC_COLLATE=)(.*)(?=;LC_CTYPE)")
+
 
 #' Setting locale
 #' @param loc string argument for sys.setlocale(), L-Serbian Latin locale,
 #' C-Serbian Cyrillic locale settings and D-for system default settings.
-#'
 #' @export
 locale <- function(loc){
   if(loc == "L"){
@@ -12,7 +11,8 @@ locale <- function(loc){
   } else if(loc == "C"){
     Sys.setlocale("LC_ALL", "Serbian (Cyrillic)_Serbia")
   } else if(loc == "D"){
-    Sys.setlocale("LC_ALL", myenv$def_loc_arg)
+    def_loc_arg <<- stringr::str_extract(myenv$def_loc, "(?<=LC_COLLATE=)(.*)(?=;LC_CTYPE)")
+    Sys.setlocale("LC_ALL", def_loc_arg)
   } else {
     stop()
   }
